f24f38545585906be4982d8a5c694ba062529872
Mike Heiser <mikeheiser@Mikes-MacBook-Air.local>
2018-03-27 19:47:14 -0700

    finished

--- server.js
+++ server.js
@@ -25,6 +25,8 @@ var routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 
+app.use(express.static("images"))
+
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
   // Log (server-side) when our server has started
