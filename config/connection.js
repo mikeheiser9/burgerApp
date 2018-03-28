27164602dff48b08d3451456226cb953b990bd81
Mike Heiser <mikeheiser@Mikes-MacBook-Air.local>
2018-03-28 14:15:12 -0700

    fixed heroku connection

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,13 @@ var mysql = require("mysql");
 
 
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+// var connection = mysql.createConnection({
+//   port: 3306,
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "burgers_db"
+// });
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
